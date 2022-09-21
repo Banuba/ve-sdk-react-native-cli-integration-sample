45293248a07cdb6496178aab350b9eb398c5e620
Gleb Prischepa <gleb.prischepa@banuba.com>
2022-09-21 18:36:09 +0300

    Implement custom audio browser approach for Android

---
+++ AudioBrowser.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet,
+  Text,
+  Button,
+  View,
+  Platform,
+  NativeModules
+} from 'react-native';
+
+const { VideoEditorModule } = NativeModules;
+
+async function applyLocalAudio() {
+  // Invokes native Android Video Editor integration module that will pass your prepared audio file to Video Editor SDK.
+  // You can override "VideoEditorModule.applyAudioTrack()" to pass your custom audio data from React Native to Android.
+  return await VideoEditorModule.applyAudioTrack();
+}
+
+// Simple Screen to show how to pass and apply audio in Video Editor SDK.
+// You can implement you own screen with browsing and downloading audio files.
+// Audio file MUST BE stored to device storage before using in Video Editor SDK.
+export default function AudioBrowser() {
+  return (
+      <View style={styles.container}>
+        <Text style={{padding: 16, textAlign: 'center'}}>
+        Local audio file is used to demonstrate how to play audio in Video Editor SDK
+        </Text>
+
+        <Button
+             title = "Use Local Audio"
+             onPress={async () => {
+                            if (Platform.OS === 'android') {
+                               applyLocalAudio()
+                            } else {
+                                console.log('Platform implementation is not supported yet!')
+                            }
+                      }
+             }/>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginVertical: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
--- android/app/src/main/java/com/vesdkreactnativecliintegrationsample/VideoEditorModule.kt
+++ android/app/src/main/java/com/vesdkreactnativecliintegrationsample/VideoEditorModule.kt
@@ -2,17 +2,25 @@ package com.vesdkreactnativecliintegrationsample
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.AssetManager
 import android.net.Uri
+import android.util.Log
 import com.banuba.sdk.cameraui.data.PipConfig
+import com.banuba.sdk.core.data.TrackData
 import com.banuba.sdk.export.data.ExportResult
 import com.banuba.sdk.export.utils.EXTRA_EXPORTED_SUCCESS
 import com.banuba.sdk.ve.flow.VideoCreationActivity
 import com.facebook.react.bridge.*
+import com.google.android.exoplayer2.upstream.cache.CacheDataSink
+import java.io.*
+import java.util.*
 
 class VideoEditorModule(reactContext: ReactApplicationContext) :
     ReactContextBaseJavaModule(reactContext) {
 
     companion object {
+        const val TAG = "VideoEditorModule"
+
         private const val EXPORT_REQUEST_CODE = 1111
         private const val E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST"
         private const val E_VIDEO_EDITOR_CANCELLED = "E_VIDEO_EDITOR_CANCELLED"
@@ -64,6 +72,9 @@ class VideoEditorModule(reactContext: ReactApplicationContext) :
 
     override fun getName(): String = "VideoEditorModule"
 
+    /**
+     * Open Video Editor SDK
+     */
     @ReactMethod
     fun openVideoEditor(inputPromise: Promise) {
         val hostActivity = currentActivity
@@ -89,5 +100,95 @@ class VideoEditorModule(reactContext: ReactApplicationContext) :
             )
             hostActivity.startActivityForResult(intent, EXPORT_REQUEST_CODE)
         }
+    }
+
+    /**
+     * Applies selected audio on custom Audio Browser in Video Editor SDK.
+     *
+     * This implementation demonstrates how to play audio stored in Android assets in Video Editor SDK.
+     *
+     * Since audio browsing and downloading logic can be implemented using React Native on JS side
+     * you can pass specific audio params in this method to build TrackData
+     * and use it in "AudioBrowserActivity.applyAudioTrack".
+     */
+    @ReactMethod
+    fun applyAudioTrack(inputPromise: Promise) {
+        val hostActivity = currentActivity
+
+        // Check if host Activity is a your specific Android Activity responsible for
+        // passing audio to Video Editor SDK i.e. AudioBrowserActivity.
+        if (hostActivity is AudioBrowserActivity) {
+            // Sample audio file used to demonstrate how to pass and play audio file
+            // in Video Editor SDK
+            val sampleAudioFileName = "sample_audio.mp3"
+            val filesStorage: File = hostActivity.applicationContext.filesDir
+            val assets: AssetManager = hostActivity.applicationContext.assets
+
+            val audioTrack: TrackData? = try {
+                // Video Editor SDK can play ONLY audio file stored on device.
+                // Make sure that you store audio file on a device before trying to play it.
+                val sampleAudioFile = prepareAudioFile(assets, filesStorage, sampleAudioFileName)
+
+                // TrackData is required in Video Editor SDK for playing audio.
+                TrackData(
+                    UUID.randomUUID(),
+                    "Set title",
+                    Uri.fromFile(sampleAudioFile),
+                    "Set artist"
+                )
+            } catch (e: IOException) {
+                Log.w(TAG, "Cannot prepare sample audio file", e)
+                // You can pass null as TrackData to cancel playing last used audio in Video Editor SDK
+                null
+            }
+
+            Log.d(TAG, "Apply audio track = $audioTrack")
+            hostActivity.applyAudioTrack(audioTrack)
+        }
+    }
+
+    /**
+     * Utils methods used to prepare sample audio track for playing in Video Editor SDK.
+     * NOT REQUIRED IN YOUR APP.
+     */
+    @Throws(IOException::class)
+    private fun prepareAudioFile(
+        assets: AssetManager,
+        filesStorage: File,
+        audioFileName: String
+    ): File {
+        val sampleAudioFile = File(filesStorage, audioFileName)
+
+        var outputStream: OutputStream? = null
+        try {
+            assets.open(audioFileName).use { inputStream ->
+                outputStream = FileOutputStream(sampleAudioFile)
+                val size = copyStream(inputStream, requireNotNull(outputStream))
+                Log.d(TAG, "Audio file has been copied. Size = $size")
+            }
+        } catch (e: IOException) {
+            throw e
+        } finally {
+            outputStream?.flush()
+            outputStream?.close()
+        }
+
+        return sampleAudioFile
+    }
+
+    @Throws(IOException::class)
+    private fun copyStream(
+        inStream: InputStream,
+        outStream: OutputStream
+    ): Long {
+        var size = 0L
+        val buffer = ByteArray(CacheDataSink.DEFAULT_BUFFER_SIZE)
+        var bytes = inStream.read(buffer)
+        while (bytes >= 0) {
+            outStream.write(buffer, 0, bytes)
+            size += bytes.toLong()
+            bytes = inStream.read(buffer)
+        }
+        return size
     }
 }

c065ddd140b6fc43da402956cafd5ef06be78fee
GlebPBanuba <67113090+GlebPBanuba@users.noreply.github.com>
2024-03-08 13:46:49 +0300

    Major docs and integration update

--- App.js
+++ App.js
@@ -9,30 +9,24 @@ import {
 } from 'react-native';
 const {SdkEditorModule} = NativeModules;
 
-// Set Banuba license token for Video Editor SDK
+// Set Banuba license token for Video and Photo Editor SDK
 const LICENSE_TOKEN = SET LICENSE TOKEN
 
-const ERR_SDK_NOT_INITIALIZED_CODE = 'ERR_SDK_EDITOR_NOT_INITIALIZED';
-const ERR_SDK_NOT_INITIALIZED_MESSAGE = 'Banuba Video Editor SDK is not initialized: license token is unknown or incorrect.\nPlease check your license token or contact Banuba';
-
-const ERR_LICENSE_REVOKED_CODE = 'ERR_SDK_EDITOR_LICENSE_REVOKED';
-const ERR_LICENSE_REVOKED_MESSAGE = 'License is revoked or expired. Please contact Banuba https://www.banuba.com/faq/kb-tickets/new';
-
 function initSDK() {
-  SdkEditorModule.initVideoEditor(LICENSE_TOKEN);
+  SdkEditorModule.initSDK(LICENSE_TOKEN);
 }
 
-async function startIosVideoEditor() {
+async function startVideoEditor() {
   initSDK();
   return await SdkEditorModule.openVideoEditor();
 }
 
-async function startIosVideoEditorPIP() {
+async function startVideoEditorPIP() {
   initSDK();
   return await SdkEditorModule.openVideoEditorPIP();
 }
 
-async function startIosVideoEditorTrimmer() {
+async function startVideoEditorTrimmer() {
   initSDK();
   return await SdkEditorModule.openVideoEditorTrimmer();
 }
@@ -40,21 +34,6 @@ async function startIosVideoEditorTrimmer() {
 async function startIosPhotoEditor() {
   await SdkEditorModule.initPhotoEditor(LICENSE_TOKEN);
   return await SdkEditorModule.openPhotoEditor();
-}
-
-async function startAndroidVideoEditorTrimmer() {
-  initSDK();
-  return await SdkEditorModule.openVideoEditorTrimmer();
-}
-
-async function startAndroidVideoEditor() {
-  initSDK();
-  return await SdkEditorModule.openVideoEditor();
-}
-
-async function startAndroidVideoEditorPIP() {
-  initSDK();
-  return await SdkEditorModule.openVideoEditorPIP();
 }
 
 async function startAndroidPhotoEditor() {
@@ -71,34 +50,16 @@ export default class App extends Component {
     };
   }
 
-  handleExportException(e) {
-    var message = '';
-    switch (e.code) {
-      case ERR_SDK_NOT_INITIALIZED_CODE:
-        message = ERR_SDK_NOT_INITIALIZED_MESSAGE;
-        break;
-      case ERR_LICENSE_REVOKED_CODE:
-        message = ERR_LICENSE_REVOKED_MESSAGE;
-        break;
-      default:
-        message = '';
-        console.log(
-          'Banuba ' +
-            Platform.OS.toUpperCase() +
-            ' Video Editor export video failed = ' +
-            e,
-        );
-        break;
-    }
-    this.setState({errorText: message});
+  handleVideoExport(response) {
+    console.log('Export completed successfully: video = ' + response?.videoUri + '; videoPreview = '
+        + response?.previewUri);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{padding: 16, textAlign: 'center'}}>
-          Sample integration of Banuba Video Editor into React Native CLI
-          project
+        <Text style={{padding: 16, textAlign: 'center', fontSize: 18 }}>
+          Sample integration of Banuba Video and Photo Editor into React Native
         </Text>
 
         <Text
@@ -142,30 +103,9 @@ export default class App extends Component {
           <Button
             title="Open Video Editor - Default"
             onPress={async () => {
-              if (Platform.OS === 'android') {
-                startAndroidVideoEditor()
-                  .then(videoUri => {
-                    console.log('Exported video = ' +videoUri,);
-                  })
-                  .catch(e => {
-                    this.handleExportException(e);
-                  });
-              } else {
-                startIosVideoEditor()
-                  .then(response => {
-                    const exportedVideoUri = response?.videoUri;
-                    const exportedVideoPreviewUri = response?.previewUri;
-                    console.log(
-                      'Banuba iOS Video Editor export video completed successfully. Video uri = '
-                      + exportedVideoUri
-                      + ' previewUri = '
-                      + exportedVideoPreviewUri,
-                    );
-                  })
-                  .catch(e => {
-                    this.handleExportException(e);
-                  });
-              }
+                 startVideoEditor()
+                    .then(response => { this.handleVideoExport(response); })
+                    .catch(e => { this.handleExportException(e); });
             }}
           />
         </View>
@@ -174,33 +114,9 @@ export default class App extends Component {
           <Button
             title="Open Video Editor - PIP"
             onPress={async () => {
-              if (Platform.OS === 'android') {
-                startAndroidVideoEditorPIP()
-                  .then(videoUri => {
-                    console.log(
-                      'Banuba Android Video Editor export video completed successfully. Video uri = ' +
-                        videoUri,
-                    );
-                  })
-                  .catch(e => {
-                    this.handleExportException(e);
-                  });
-              } else {
-                startIosVideoEditorPIP()
-                  .then(response => {
-                    const exportedVideoUri = response?.videoUri;
-                    const exportedVideoPreviewUri = response?.previewUri;
-                    console.log(
-                      'Banuba iOS Video Editor export video completed successfully. Video uri = '
-                      + exportedVideoUri
-                      + ' previewUri = '
-                      + exportedVideoPreviewUri,
-                    );
-                  })
-                  .catch(e => {
-                    this.handleExportException(e);
-                  });
-              }
+                startVideoEditorPIP()
+                    .then(response => { this.handleVideoExport(response); })
+                    .catch(e => { this.handleExportException(e); });
             }}
           />
         </View>
@@ -209,39 +125,43 @@ export default class App extends Component {
           <Button
             title="Open Video Editor - Trimmer"
             onPress={async () => {
-              if (Platform.OS === 'android') {
-                startAndroidVideoEditorTrimmer()
-                  .then(videoUri => {
-                    console.log(
-                      'Banuba Android Video Editor export video completed successfully. Video uri = ' +
-                        videoUri,
-                    );
-                  })
-                  .catch(e => {
-                    this.handleExportException(e);
-                  });
-              } else {
-                startIosVideoEditorTrimmer()
-                  .then(response => {
-                    const exportedVideoUri = response?.videoUri;
-                    const exportedVideoPreviewUri = response?.previewUri;
-                    console.log(
-                      'Banuba iOS Video Editor export video completed successfully. Video uri = '
-                      + exportedVideoUri
-                      + ' previewUri = '
-                      + exportedVideoPreviewUri,
-                    );
-                  })
-                  .catch(e => {
-                    this.handleExportException(e);
-                  });
-              }
+                startVideoEditorTrimmer()
+                    .then(response => { this.handleVideoExport(response); })
+                    .catch(e => { this.handleExportException(e); });
             }}
           />
         </View>
       </View>
     );
   }
+
+  handleExportException(e) {
+      var message = '';
+      switch (e.code) {
+        case 'ERR_CODE_NOT_INITIALIZED':
+          message = 'Banuba Video Editor SDK is not initialized: license token is unknown or incorrect.\nPlease check your license token or contact Banuba';
+          break;
+        case 'ERR_CODE_LICENSE_REVOKED':
+          message = 'License is revoked or expired. Please contact Banuba https://www.banuba.com/faq/kb-tickets/new';
+          break;
+        case 'ERR_MISSING_EXPORT_RESULT':
+          message = 'Missing video export result!';
+        case 'ERR_CODE_NO_HOST_CONTROLLER':
+          message = "Host Activity or ViewController does not exist!";
+        case 'ERR_VIDEO_EXPORT_CANCEL':
+          message = "Video export is canceled";
+        default:
+          message = '';
+          console.log(
+            'Banuba ' +
+              Platform.OS.toUpperCase() +
+              ' Video Editor export video failed = ' +
+              e,
+          );
+          break;
+      }
+      this.setState({errorText: message});
+    }
 }
 
 const styles = StyleSheet.create({
---
+++ mddocs/quickstart_pe_android.md
@@ -0,0 +1,126 @@
+# Android Photo Editor SDK quickstart
+
+This guide demonstrates how to quickly integrate Android Photo Editor SDK into your React Native project.
+The main part of an integration and customization is implemented in ```android``` directory
+in your React Native project using native Android development process.
+
+Once complete you will be able to launch photo editor in your React Native project.
+
+- [Installation](#Installation)
+- [Launch](#Launch)
+- [What is next?](#What-is-next)
+
+## Installation
+GitHub Packages is used for downloading Android Photo Editor SDK modules.
+First, add repositories to [gradle](../android/build.gradle#L28) file in ```allprojects``` section.
+
+```groovy
+...
+
+allprojects {
+    repositories {
+        ...
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Banuba/banuba-ve-sdk")
+            credentials {
+                username = "Banuba"
+                password = "\u0038\u0036\u0032\u0037\u0063\u0035\u0031\u0030\u0033\u0034\u0032\u0063\u0061\u0033\u0065\u0061\u0031\u0032\u0034\u0064\u0065\u0066\u0039\u0062\u0034\u0030\u0063\u0063\u0037\u0039\u0038\u0063\u0038\u0038\u0066\u0034\u0031\u0032\u0061\u0038"
+            }
+        }
+        maven {
+            name = "ARCloudPackages"
+            url = uri("https://maven.pkg.github.com/Banuba/banuba-ar")
+            credentials {
+                username = "Banuba"
+                password = "\u0038\u0036\u0032\u0037\u0063\u0035\u0031\u0030\u0033\u0034\u0032\u0063\u0061\u0033\u0065\u0061\u0031\u0032\u0034\u0064\u0065\u0066\u0039\u0062\u0034\u0030\u0063\u0063\u0037\u0039\u0038\u0063\u0038\u0038\u0066\u0034\u0031\u0032\u0061\u0038"
+            }
+        }
+        maven {
+            name "GitHubPackagesEffectPlayer"
+            url "https://maven.pkg.github.com/sdk-banuba/banuba-sdk-android"
+            credentials {
+                username = "sdk-banuba"
+                password = "\u0067\u0068\u0070\u005f\u0033\u0057\u006a\u0059\u004a\u0067\u0071\u0054\u0058\u0058\u0068\u0074\u0051\u0033\u0075\u0038\u0051\u0046\u0036\u005a\u0067\u004f\u0041\u0053\u0064\u0046\u0032\u0045\u0046\u006a\u0030\u0036\u006d\u006e\u004a\u004a"
+            }
+        }
+
+        ...
+    }
+}
+```
+
+Specify Photo Editor SDK dependencies in the app [gradle](../android/app/build.gradle#L170) file.
+```groovy
+    def banubaPESdkVersion = '1.1.0'
+    implementation "com.banuba.sdk:pe-sdk:${banubaPESdkVersion}"
+
+    def banubaSdkVersion = '1.34.0'
+    implementation "com.banuba.sdk:core-sdk:${banubaSdkVersion}"
+    implementation "com.banuba.sdk:core-ui-sdk:${banubaSdkVersion}"
+    implementation "com.banuba.sdk:ve-gallery-sdk:${banubaSdkVersion}"
+    implementation "com.banuba.sdk:effect-player-adapter:${banubaSdkVersion}"
+```
+
+Additionally, make sure the following plugins are in your app [gradle](../android/app/build.gradle#L2) file.
+```groovy
+apply plugin: 'com.android.application'
+apply plugin: 'kotlin-android'
+apply plugin: 'kotlin-parcelize'
+```
+
+## Launch
+
+Create [BanubaSdkReactPackage](../android/app/src/main/java/com/vesdkreactnativecliintegrationsample/BanubaSdkReactPackage.kt) and add [SdkEditorModule](../android/app/src/main/java/com/vesdkreactnativecliintegrationsample/SdkEditorModule.kt) to the list of modules.
+```kotlin
+ class BanubaSdkReactPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+        val modules = mutableListOf<NativeModule>()
+        modules.add(SdkEditorModule(reactContext))
+        return modules
+    }
+    ...
+}
+```
+
+Next, add ```BanubaSdkReactPackage```  to the list of packages in [Application](../android/app/src/main/java/com/vesdkreactnativecliintegrationsample/MainApplication.java#L26) class
+```java
+        @Override
+        protected List<ReactPackage> getPackages() {
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            packages.add(new BanubaSdkReactPackage());
+            ...
+            return packages;
+        }
+```
+
+
+[Promises](https://reactnative.dev/docs/native-modules-android#promises) feature is used to make a bridge between React Native and Android.
+
+Invoke [initSDK](../App.js#L15) on React Native side to initialize SDK with the license token.
+```javascript
+SdkEditorModule.initSDK(LICENSE_TOKEN);
+```
+
+Add [ReactMethod](../android/app/src/main/java/com/vesdkreactnativecliintegrationsample/SdkEditorModule.kt#L116) on Android side to initialize Video Editor SDK.
+
+:exclamation: Important
+1. Instance ```editorSDK``` is ```null``` if the license token is incorrect. In this case you cannot use photo editor. Check your license token.
+2. It is highly recommended to [check license](../android/app/src/main/java/com/vesdkreactnativecliintegrationsample/SdkEditorModule.kt#L318) if the license is active before starting Photo Editor.
+
+Finally, once the SDK in initialized you can invoke [startAndroidPhotoEditor](../App.js#L39) message from React Native to Android
+
+```javascript
+await SdkEditorModule.openPhotoEditor();
+```
+
+and add [ReactMethod](../android/app/src/main/java/com/vesdkreactnativecliintegrationsample/SdkEditorModule.kt#L166) on Android side to start Photo Editor.
+
+
+## What is next?
+This quickstart guide has just covered how to quickly integrate Android Photo Editor SDK,
+it is considered you managed to start photo editor from your React Native project.
+
+Please check out [docs](https://docs.banuba.com/ve-pe-sdk/docs/android/requirements-pe/) to know more about the SDK and complete full integration.
